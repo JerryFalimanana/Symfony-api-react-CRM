980bfc55c44c260d7aaa130bc85dc7b49f32953d
jerryfalimanana <ratsimbajaona1006@gmail.com>
2022-04-19 09:55:13 +0300

    Add chrono property for invoice

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,6 +13,7 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
+        $chrono = 1;
 
         for ($c = 0; $c < 30; $c++) {
             $customer = new Customer();
@@ -28,8 +29,10 @@ class AppFixtures extends Fixture
                 $invoice->setAmount($faker->randomFloat(2, 250, 5000))
                         ->setSentAt($faker->dateTimeBetween('-6 months'))
                         ->setStatus($faker->randomElement(['SENT', 'PAID', 'CANCELLED']))
+                        ->setChrono($chrono)
                         ->setCustomer($customer);
                 
+                $chrono++;
                 $manager->persist($invoice);
             }
         }
