1a0adf5020145c85303c0b12ba74c65e7425b9dd
jerryfalimanana <ratsimbajaona1006@gmail.com>
2022-06-22 08:48:43 +0300

    build navbar and homepage

---
+++ assets/js/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+const HomePage = (props) => {
+    return ( 
+        <div className="jumbotron mt-3">
+            <h1 className="display-3">Bienvenue sur SymfonyApp</h1>
+                <p className="lead">Nous mettons en relation les voyageurs et les propriétaire qui veulent profiter d'opportunités et gagner de l'argent en proposant leurs bien à la location.</p>
+            <hr className="my-4"/>
+                <p>Vous êtes voyageurs ? Et vous voulez voir où vous pourrez loger lors de votre prochain déplacement ?</p>
+            <p className="lead">
+                <a className="btn btn-primary btn-lg" href="#" role="button">Voir nos annonces</a>
+            </p>
+        </div> 
+    );
+}
+ 
+export default HomePage;
