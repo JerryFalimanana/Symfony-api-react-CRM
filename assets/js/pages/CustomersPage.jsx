3ef2f7afbe73a9dcff7d9efeb0d9b974aabe41e1
jerryfalimanana <ratsimbajaona1006@gmail.com>
2022-06-28 11:44:41 +0300

    get customers from our API

--- assets/js/pages/CustomersPage.jsx
+++ assets/js/pages/CustomersPage.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 
 const CustomersPage = (props) => {
+    const [customers, setCustomers] = useState([]);
+
+    useEffect(() => {
+        Axios
+            .get("http://127.0.0.1:8000/api/customers")
+            .then(response => response.data["hydra:member"])
+            .then(data => setCustomers(data))
+            .catch(erron => console.log(error.response));
+    }, [])
+
     return (
         <>
             <h1>Liste des clients</h1>
@@ -17,23 +28,25 @@ const CustomersPage = (props) => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>12</td>
-                        <td>
-                            <a href="#">Jerry Falimanana</a>
-                        </td>
-                        <td>
-                            jerry@1234@yopmail.com
-                        </td>
-                        <td>Jerry Inc</td>
-                        <td className='text-center'>
-                            <span className="badge bg-success">4</span>
-                        </td>
-                        <td className='text-center'>2 300,00 €</td>
-                        <td>
-                            <button className="btn btn-sm btn-danger">Supprimer</button>
-                        </td>
-                    </tr>
+                    {customers.map(customer => 
+                        <tr key={customer.id}>
+                            <td> {customer.id} </td>
+                            <td>
+                                <a href="#">{customer.firstName} {customer.lastName}</a>
+                            </td>
+                            <td> {customer.email} </td>
+                            <td>{customer.company}</td>
+                            <td className='text-center'>
+                                <span className="badge bg-success">
+                                    {customer.invoices.length}
+                                </span>
+                            </td>
+                            <td className='text-center'>{customer.totalAmount.toLocaleString()} €</td>
+                            <td>
+                                <button className="btn btn-sm btn-danger">Supprimer</button>
+                            </td>
+                        </tr>
+                    )}
                 </tbody>
             </table>
         </>
