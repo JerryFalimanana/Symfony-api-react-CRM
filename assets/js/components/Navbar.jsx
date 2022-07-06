33c174cfc514f15aeda5934b4f8c2917c8e25297
jerryfalimanana <ratsimbajaona1006@gmail.com>
2022-07-06 17:30:45 +0300

    placement of the real link in the navigation bar

--- assets/js/components/Navbar.jsx
+++ assets/js/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import AuthAPI from '../services/authAPI';
 
 const Navbar = (props) => {
@@ -9,7 +10,7 @@ const Navbar = (props) => {
     return ( 
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
-            <a className="navbar-brand" href="#">Mon CRM</a>
+            <NavLink className="navbar-brand" to="/">Mon CRM</NavLink>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -17,18 +18,18 @@ const Navbar = (props) => {
             <div className="collapse navbar-collapse" id="navbarColor01">
                 <ul className="navbar-nav me-auto">
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Clients</a>
+                        <NavLink className="nav-link" to="/customers">Clients</NavLink>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Factures</a>
+                        <NavLink className="nav-link" to="/invoices">Factures</NavLink>
                     </li>
                 </ul>
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
-                        <a href="#" className="nav-link">Inscription</a>
+                        <NavLink to="/register" className="nav-link">Inscription</NavLink>
                     </li>
                     <li className="nav-item">
-                        <a href="#" className="btn btn-success">Connexion</a>
+                        <NavLink to="/login" className="btn btn-success">Connexion</NavLink>
                     </li>
                     <li className="nav-item">
                         <button onClick={handleLogout} className="btn btn-danger">Déconnexion</button>
